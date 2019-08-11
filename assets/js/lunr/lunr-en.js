94d5d20118493e4bc13c174f911676671276f513
HMZ <38891601+hmz777@users.noreply.github.com>
2019-08-11 13:30:15 +0300

    Update lunr-en.js

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
           '<div class="list__item">'+
             '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
               '<h2 class="archive__item-title" itemprop="headline">'+
-                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+                '<a href="'+store[ref].url.substr(store[ref].url.indexOf('/'),store[ref].url.length)+'" rel="permalink">'+store[ref].title+'</a>'+
               '</h2>'+
               '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
             '</article>'+
@@ -74,4 +74,3 @@ $(document).ready(function() {
   });
 });
 //'<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
-//'+store[ref].url+'
